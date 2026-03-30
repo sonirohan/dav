59472d58fcc7a4beaa91e15fd0587cd9fe7b1202
Rohan Soni <the.bruns.constant@gmail.com>
2026-03-30 15:22:11 -0700

    added rst to clock_divider

--- lab3/clock_divider.sv
+++ lab3/clock_divider.sv
@@ -4,18 +4,24 @@ module clock_divider#(
     parameter DIVISOR = 4
    )(
    input logic clk_in, //input 100 MHZ
-   output logic pulse_out = 1'b0 // output 25 MHZ
+   input logic rst,
+   output logic pulse_out // output 25 MHZ
     );
     logic [$clog2(DIVISOR)-1: 0] counter;
     
     always @(clk_in) begin // on every clock edge so Divisor is 4, we will have 4 clock edges before we toggle pulse_out
-
-        if(counter == DIVISOR - 1) begin
-            pulse_out <= !pulse_out;
+        if (rst) begin
             counter <= 0;
-        end else begin
-            counter <= counter + 1;
-        end 
+            pulse_out <= 0;
+        end
+        else begin
+            if(counter == DIVISOR - 1) begin
+                pulse_out <= !pulse_out;
+                counter <= 0;
+            end else begin
+                counter <= counter + 1;
+            end 
+        end
      end
     
 endmodule
--- lab3/top.sv
+++ lab3/top.sv
@@ -1,5 +1,18 @@
 module top (
-); 
-    
 
+    input logic clk, // 100 MHz clock from the crystal
+    input logic rst, // active-high reset signal from the button
+    output logic hsync, // horizontal sync signal to VGA
+    output logic vsync, // vertical sync signal to VGA
+    output logic [3:0] red, // 4-bit red signal to VGA
+    output logic [3:0] green, // 4-bit green signal to VGA
+    output logic [3:0] blue // 4-bit blue signal to VGA
+   );
+
+   logic vgaclk; // 25 MHz clock for VGA timing
+
+    clock_divider our_divider (
+         .clk_in(clk),
+         .pulse_out(vgaclk)
+    );
 endmodule
