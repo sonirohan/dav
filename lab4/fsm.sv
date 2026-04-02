5ac7cc690613a006d3b715a8775a164face0e08a
David Farag <davidfarag720@gmail.com>
2026-04-01 22:30:02 -0700

    Improvements to the fsm but currently has issues, check todo

--- lab4/fsm.sv
+++ lab4/fsm.sv
@@ -21,45 +21,79 @@ module fsm(
 localparam w_0 = 32'b01111111111111110000000000000000;
 localparam w_1 = 32'b00000000000000001000000000000000;
 
+typedef enum logic {idle, stage_1, stage_2, done} state_t;
+state_t state;
+
 logic [31:0] mid_0;
 logic [31:0] mid_1;
 logic [31:0] mid_2;
 logic [31:0] mid_3;
 
-// top left
+logic [31:0] bfu_0_in_a;
+logic [31:0] bfu_0_in_b;
+logic [31:0] bfu_1_in_a;
+logic [31:0] bfu_1_in_b;
+
+logic [31:0] bfu_0_out_a;
+logic [31:0] bfu_0_out_b;
+logic [31:0] bfu_1_out_a;
+logic [31:0] bfu_1_out_b;
+
+// top
 butterfly_unit clanker_0(
-    .a(time_signal_a),
-    .b(time_signal_c),
+    .a(bfu_0_in_a),
+    .b(bfu_0_in_b),
     .w(w_0),
-    .out1(mid_0),
-    .out2(mid_1)
+    .out1(bfu_0_out_a),
+    .out2(bfu_0_out_b)
 );
 
-// bottom left
+// bottom
 butterfly_unit clanker_1(
-    .a(time_signal_b),
-    .b(time_signal_d),
+    .a(bfu_1_in_a),
+    .b(bfu_1_in_b),
     .w(w_0),
-    .out1(mid_2),
-    .out2(mid_3)
+    .out1(bfu_1_out_a),
+    .out2(bfu_1_out_b)
 );
 
-// top right
-butterfly_unit clanker_2(
-    .a(mid_0),
-    .b(mid_2),
-    .w(w_0),
-    .out1(freq_signal_a),
-    .out2(freq_signal_c)
-);
+// TODO FIX THE W INPUTS AND FIGURE OUT STATE MACHINE VALIDITY
 
-// bottom right
-butterfly_unit clanker_3(
-    .a(mid_1),
-    .b(mid_3),
-    .w(w_1),
-    .out1(freq_signal_b),
-    .out2(freq_signal_d)
-);
+always_comb begin
+    if(state == stage_1) begin
+        bfu_0_in_a = time_signal_a;
+        bfu_0_in_b = time_signal_c;
+        mid_0 = bfu_0_out_a;
+        mid_1 = bfu_0_out_b;
+        bfu_1_in_a = time_signal_b;
+        bfu_1_in_b = time_signal_d;
+        mid_2 = bfu_1_out_a;
+        mid_3 = bfu_1_out_b;
+    end else if(state == stage_2) begin
+        bfu_0_in_a = mid_0;
+        bfu_0_in_b = mid_2;
+        freq_signal_a = bfu_0_out_a;
+        freq_signal_c = bfu_0_out_b;
+        bfu_1_in_a = mid_1;
+        bfu_1_in_b = mid_3;
+        freq_signal_b = bfu_1_out_a;
+        freq_signal_d = bfu_1_out_b;
+    end
+end
+
+always @(posedge clk) begin
+    if(rst) begin
+        state <= idle;
+        completed <= 0;
+    end else begin
+        if(start && state == idle) state <= stage_1; 
+        if(state == stage_1) state <= stage_2;
+        if(state == stage_2) state <= done;
+    end
+    if(state == done)
+    completed = 1;
+
+end
+
 
 endmodule
